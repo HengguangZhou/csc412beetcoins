3e2c223870dcbac4ae455e5b2fb4fa5da9a8a924
Brendan Kan <brendan0110901@gmail.com>
2021-04-23 00:10:06 -0700

    Changed getitem again

--- maestro_dataset.py
+++ maestro_dataset.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import pretty_midi
-from convert import convert_3d_to_2d
+from convert import convert_3d_to_2d, midi_to_piano_roll
 from dataset import get_concat_mask, get_mask
 import pickle
 import json
@@ -25,8 +25,7 @@ class MaestroDataset(Dataset):
         return len(self.midi_lst)
 
     def __getitem__(self, idx):
-        seg_lst = self.separate_midi_into_classes(self.midi_lst[idx], self.composer_lst[idx])
-        info = random.choice(seg_lst)
+        info = self.obtain_piano_rolls(self.midi_lst[idx], self.composer_lst[idx])
         # Return the random data segment
         oh_data = torch.from_numpy(info[0]).float()
         data = info[1]
@@ -75,41 +74,42 @@ class MaestroDataset(Dataset):
 
         return composer_map
 
-    def separate_midi_into_classes(self, midi_name, composer_name):
-        lst = []
-        sep_piano_roll3d = self.separate_piano_roll_by_ts(midi_name, composer_name)
+    def obtain_piano_rolls(self, midi_name, composer_name):
+        # sep_piano_roll3d = self.separate_piano_roll_by_ts(midi_name, composer_name)
+        piano_roll3d = midi_to_piano_roll(self.file_dir+'/'+midi_name, self.min_pitch,
+                                          self.max_pitch, self.timestep_len)
         # from the list of 3d piano roll, obtain the 2d piano roll
-        for i in sep_piano_roll3d:
-            piano_roll2d = convert_3d_to_2d(i, 0, self.timestep_len)  # Output is already a tensor
-            lst.append((i, piano_roll2d, composer_name))
+        piano_roll2d = convert_3d_to_2d(piano_roll3d, 0, self.timestep_len)  # Output is already a tensor
+        info = (piano_roll3d, piano_roll2d, composer_name)
 
-        return lst
+        return info
 
-    def separate_piano_roll_by_ts(self, midi_name, composer_name):
-        pm = pretty_midi.PrettyMIDI(self.file_dir + '/' + midi_name)
-        temp = []
-        for ins in pm.instruments:
-            ins_p = ins.get_piano_roll(fs=5)
-            ins_p[ins_p > 0] = 1.0
-            total_ts = ins_p.shape[1]
-            # cut off the extra length at the end to make it multiple of self.timestep_len if needed
-            if total_ts % self.timestep_len != 0:
-                ins_p = ins_p[:, 0:total_ts - (total_ts % self.timestep_len)]
-            temp.append(ins_p[self.min_pitch:self.max_pitch+1, :].transpose())
-
-        if len(temp) <= 4:
-            final_piano_roll = np.stack(temp, axis=0)
-            diff = 4 - len(temp)
-            if diff > 0:  # Pad extra layers of zeros for the final piano roll if less than 4 levels
-                final_piano_roll = np.concatenate([final_piano_roll,
-                                                   np.zeros((diff, final_piano_roll.shape[1], self.pitch_range))], axis=0)
-        else:  # More than 4 tracks, so choose 4 tracks only
-            final_piano_roll = np.stack(temp[0:4], axis=0)
-
-        # Split the whole piano roll into chunks of desired length
-        ins_p_lst = np.split(final_piano_roll, final_piano_roll.shape[1] // self.timestep_len, axis=1)
-
-        return ins_p_lst
+    # def separate_piano_roll_by_ts(self, midi_name, composer_name):
+    #     pm = pretty_midi.PrettyMIDI(self.file_dir + '/' + midi_name)
+    #     temp = []
+    #     for ins in pm.instruments:
+    #         ins_p = ins.get_piano_roll(fs=5)
+    #         ins_p[ins_p > 0] = 1.0
+    #         total_ts = ins_p.shape[1]
+    #         # cut off the extra length at the end to make it multiple of self.timestep_len if needed
+    #         if total_ts % self.timestep_len != 0:
+    #             ins_p = ins_p[:, 0:total_ts - (total_ts % self.timestep_len)]
+    #         temp.append(ins_p[self.min_pitch:self.max_pitch+1, :].transpose())
+    #
+    #     if len(temp) <= 4:
+    #         final_piano_roll = np.stack(temp, axis=0)
+    #         diff = 4 - len(temp)
+    #         if diff > 0:  # Pad extra layers of zeros for the final piano roll if less than 4 levels
+    #             final_piano_roll = np.concatenate([final_piano_roll,
+    #                                                np.zeros((diff, final_piano_roll.shape[1],
+    #                                                self.pitch_range))], axis=0)
+    #     else:  # More than 4 tracks, so choose 4 tracks only
+    #         final_piano_roll = np.stack(temp[0:4], axis=0)
+    #
+    #     # Split the whole piano roll into chunks of desired length
+    #     ins_p_lst = np.split(final_piano_roll, final_piano_roll.shape[1] // self.timestep_len, axis=1)
+    #
+    #     return ins_p_lst
 
 
 if __name__ == '__main__':
